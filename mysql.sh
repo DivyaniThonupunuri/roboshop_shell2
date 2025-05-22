c50c40f40d73cd957e0255b797692ec845a7789e
Divyani <divyani.t74@gmail.com>
2025-05-22 20:41:58 +0530

    common

--- mysql.sh
+++ mysql.sh
@@ -21,7 +21,6 @@ VALIDATE $? "Starting MySQL"
 mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD &>>$LOG_FILE
 VALIDATE $? "Setting MySQL root password"
 
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+
+
