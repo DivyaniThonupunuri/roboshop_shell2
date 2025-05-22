f23e6d801d4cbd7804253bb18e8119e5ed4fe68f
Divyani <divyani.t74@gmail.com>
2025-05-22 20:35:46 +0530

    common

--- common.sh
+++ common.sh
@@ -87,7 +87,7 @@ VALIDATE $? "Installing Dependencies"
 
 print_time(){
     END_TIME=$( date +%s )
-TOTAL_TIME=$(( $END_TIME  -  $START_TIME ))
+TOTAL_TIME=$(($END_TIME - $START_TIME))
 
 echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
 }
